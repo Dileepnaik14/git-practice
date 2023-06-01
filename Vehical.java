0e398f08bc49d9bdf287c20036ca36fa8d40f135
Dileepnaik14 <dileepnaik1444@gmail.com>
2023-06-01 16:08:37 +0530

    method called

--- Vehical.java
+++ Vehical.java
@@ -2,8 +2,16 @@ public interface Vehical {
 void start();
 }
 
-class ride{
+class car{
     public void start(){
         System.out.println("Car has been started")
     }
 }
+
+class Ride{
+    public static void main(String [] args){
+        Vehical v=new Vehical();
+        v.start();
+    }
+}
+
